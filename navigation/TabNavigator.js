899d3c6457c4eb56bfe6a9c5570df30aee478d77
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2020-01-10 23:40:32 +0100

    navbar over map fixed

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React from 'react'
 import { Dimensions } from 'react-native'
 
 import { createAppContainer } from 'react-navigation'
@@ -12,50 +12,51 @@ import MapScreen from '../screens/MapScreen'
 import InfoScreen from '../screens/InfoScreen'
 
 const screenWidth = Math.round(Dimensions.get('window').width)
-const horizontalMargin = (screenWidth-270) / 2
+const horizontalMargin = (screenWidth - 270) / 2
 
 const AppNavigator = createBottomTabNavigator({
     Map: {
         screen: MapScreen,
         navigationOptions: {
-            tabBarIcon: ({tintColor}) => (<Feather name='map' size={24} color={tintColor}/>),
+            tabBarIcon: ({ tintColor }) => (<Feather name='map' size={24} color={tintColor} />),
         },
     },
     Profile: {
         screen: ProfileScreen,
         navigationOptions: {
-            tabBarIcon: ({tintColor}) => (<MaterialIcons name='person' size={24} color={tintColor}/>)
+            tabBarIcon: ({ tintColor }) => (<MaterialIcons name='person' size={24} color={tintColor} />)
         }
     },
     Info: {
         screen: InfoScreen,
         navigationOptions: {
-            tabBarIcon: ({tintColor}) => (<Feather name='info' size={24} color={tintColor}/>)
+            tabBarIcon: ({ tintColor }) => (<Feather name='info' size={24} color={tintColor} />)
         }
     },
-  }, {
-    initialRouteName: 'Map',
-    tabBarOptions: {
-        inactiveTintColor: Colors.grey,
-        activeTintColor: Colors.mainTint,
-        style:{
-            backgroundColor: '#FFFFFF',
-            width: 270,
-            height: 60,
-            borderTopColor: 'transparent',
-            shadowColor: 'rgba(0, 0, 0, 0.25)',
-            shadowOffset: { width: 4, height: 0},
-            shadowRadius: 4,
-            shadowOpacity: 1,
-            borderRadius: 46,
-            display: 'flex',
-            marginBottom: '7%',
-            marginHorizontal: horizontalMargin
+}, {
+        initialRouteName: 'Map',
+        tabBarOptions: {
+            inactiveTintColor: Colors.grey,
+            activeTintColor: Colors.mainTint,
+            style: {
+                backgroundColor: '#FFFFFF',
+                width: 270,
+                height: 60,
+                position: 'absolute',
+                borderTopColor: 'transparent',
+                shadowColor: 'rgba(0, 0, 0, 0.25)',
+                shadowOffset: { width: 4, height: 0 },
+                shadowRadius: 4,
+                shadowOpacity: 1,
+                borderRadius: 46,
+                display: 'flex',
+                marginBottom: '7%',
+                marginHorizontal: horizontalMargin
+            },
+            showLabel: false,
         },
-        showLabel: false,
     },
-    },
-    
+
 )
 
 const TabNavigator = createAppContainer(AppNavigator)
