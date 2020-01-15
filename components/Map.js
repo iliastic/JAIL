4bc692b5d1425787fd087becccf417f2efaa5426
JanHaerensBeCode <haerens.jan@gmail.com>
2020-01-15 16:14:36 +0100

    restructure mapcomponents/ dataflow + onclick marker add card

---
+++ components/Map.js
@@ -0,0 +1,220 @@
+import React, {useEffect, useState} from 'react'
+import { Button, StyleSheet } from 'react-native'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+
+import markerIcon from '../assets/marker.png'
+import ShopInfo from './ShopInfo'
+
+const Map = props => {
+    const [markerId, setMarkerId] = useState('')
+    const [markerData, setMarkerData] = useState(undefined)
+
+    useEffect(() => {
+        let markerState = props.data.find(target => {
+            return target.key === markerId
+        })
+        setMarkerData(markerState)
+    })
+
+    return (
+        <MapView 
+            initialRegion={{
+                latitude: 51.2182,
+                longitude: 4.42038,
+                latitudeDelta: 0.08,
+                longitudeDelta: 0.08,
+            }}
+            style={styles.map}
+            customMapStyle={generatedMap}
+            provider={PROVIDER_GOOGLE}
+            >
+            {props.data.map(info => {
+                return (
+                    <Marker 
+                        coordinate={{latitude: info.lat, longitude: info.long}}
+                        image={markerIcon}
+                        key={info.key}
+                        onPress={() => {
+                            setMarkerId(info.key)          
+                        }}
+                    >
+                    </Marker>
+                )
+            })}
+            {markerData !== undefined &&
+                <ShopInfo data={markerData} />
+            }
+        </MapView>
+    )}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+})
+
+const generatedMap = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    }
+  ]
+
+  export default Map
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,17 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet} from 'react-native'
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import { StyleSheet, View, Text, Dimensions, Button} from 'react-native'
 
 import firebase from '../database/Firebase'
-
-import markerIcon from '../assets/marker.png'
+import Map from '../components/Map'
 
 const MapScreen = () => {
-    const ref = firebase.firestore().collection('coffeeShops')
+  const ref = firebase.firestore().collection('coffeeShops')
 
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
 
   useEffect(() => {
+
     return ref.onSnapshot(snapshot => {
       const list = []
       snapshot.forEach(doc => {
@@ -42,37 +41,15 @@ const MapScreen = () => {
         setLoading(false)
       }
     })
+    
   }, [])
 
   if (loading) {
       return null
   }
-
-    return (
-        <MapView 
-        initialRegion={{
-            latitude: 51.2182,
-            longitude: 4.42038,
-            latitudeDelta: 0.08,
-            longitudeDelta: 0.08,
-          }}
-          style={styles.map}
-          customMapStyle={generatedMap}
-          provider={PROVIDER_GOOGLE}
-        >
-            {data.map(marker => {
-                return (
-                    <Marker 
-                        coordinate={{latitude: marker.lat, longitude: marker.long}}
-                        title='werk pls'
-                        description= 'plsplspls'
-                        image={markerIcon}
-                        key={marker.key}
-                    />
-                )
-            })}
-        </MapView>
-    )
+  return (
+    <Map data={data}/>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -81,172 +58,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    map: {
-        ... StyleSheet.absoluteFillObject    
-    }
-    
+    },    
 })
-
-const generatedMap = [
-    {
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.stroke",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#bdbdbd"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#ffffff"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#dadada"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "featureType": "road.local",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.line",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#c9c9c9"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    }
-  ]
 
 export default MapScreen
