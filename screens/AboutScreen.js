66305fbbeb81295ad0641a7f86a94e3e632cce52
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2020-02-04 19:56:55 +0100

    fix constant white space top while scrolling

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -9,59 +9,51 @@ const AboutScreen = ({ navigation }) => {
             </TouchableOpacity>
             <Image style={styles.backgroundImage} source={require('../assets/info.png')} style={styles.backgroundImage} />
             <Image style={styles.infoLogo} source={require('../assets/infologo.png')} />
-            <ScrollView style={styles.scrollcontainer}>
-                <Text style={styles.h1}>
-                    About Cupp-a.
+            <View style={styles.scrollContainer}>
+                <ScrollView style={styles.scrollMargin}>
+                    <Text style={styles.h1}>
+                        About Cupp-a.
                 </Text>
-                <Text style={styles.alinea}>
-                    This application is made to give you an overview of the different coffee bars in Antwerp
-                    and their sustainability contribution. Each coffee bar is rated on 7 levels based on interviews
-                    and research. Going from the kind of coffee beans to their waste management. The goal of this app
+                    <Text style={styles.alinea}>
+                        This application is made to give you an overview of the different coffee bars in Antwerp
+                        and their sustainability contribution. Each coffee bar is rated on 7 levels based on interviews
+                        and research. Going from the kind of coffee beans to their waste management. The goal of this app
                     is to raise awareness, learn from each other and to grow together. {"\n"}
-                </Text>
-                <Text style={styles.alinea}>
-                    This app is made for the consumer to make an informed choice of where to purchase your cup of coffee.
+                    </Text>
+                    <Text style={styles.alinea}>
+                        This app is made for the consumer to make an informed choice of where to purchase your cup of coffee.
                     But also for the coffee shops to learn from each other and improve. {"\n"}
-                    So buy your cup of coffee wisely
+                        So buy your cup of coffee wisely
                 </Text>
-                <Text style={styles.alinea}>
-                    This app is made possible by a collaboration between students from Antwerp Management School
-                    (AMS) and BeCode Antwerp. The BeCode team is responsible for the development and design of the
+                    <Text style={styles.alinea}>
+                        This app is made possible by a collaboration between students from Antwerp Management School
+                        (AMS) and BeCode Antwerp. The BeCode team is responsible for the development and design of the
                     application. While the AMS team gathered data from different coffee bars around Antwerp. {"\n"}
-                </Text>
-                <Text style={styles.team}>
-                    Developer team {'\n'}
-                </Text>
-                <Text style={styles.members}>
-                    Jan Haerens {'\n'}
-                    Ilias Mesbahi {'\n'}
-                    Panadda Manarata {'\n'}
-                    Hicham Bezzizi {'\n'}
-                    Ilger Depraetere {'\n'}
-                </Text>
-                <Text style={styles.team}>
-                    AMS team {'\n'}
-                </Text>
-                <Text style={styles.members}>
-                    Dhanvanth Raman {'\n'}
-                    Matisse Verboven {'\n'}
-                    Nicole  Durosko {'\n'}
-                    Pauwel Derammelaere {'\n'}
-                    Sofie Lambert {'\n'}
-                    Stephan De Vries {'\n'}
-                    Vincent Chang {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                    {'\n'}
-                </Text>
-            </ScrollView>
+                    </Text>
+                    <Text style={styles.team}>
+                        Developer team {'\n'}
+                    </Text>
+                    <Text style={styles.members}>
+                        Jan Haerens {'\n'}
+                        Ilias Mesbahi {'\n'}
+                        Panadda Manarata {'\n'}
+                        Hicham Bezzizi {'\n'}
+                        Ilger Depraetere {'\n'}
+                    </Text>
+                    <Text style={styles.team}>
+                        AMS team {'\n'}
+                    </Text>
+                    <Text style={styles.members}>
+                        Dhanvanth Raman {'\n'}
+                        Matisse Verboven {'\n'}
+                        Nicole  Durosko {'\n'}
+                        Pauwel Derammelaere {'\n'}
+                        Sofie Lambert {'\n'}
+                        Stephan De Vries {'\n'}
+                        Vincent Chang {'\n'}
+                    </Text>
+                </ScrollView>
+            </View>
         </View>
     )
 }
@@ -70,9 +62,8 @@ const styles = StyleSheet.create({
     container: {
         margin: 20,
     },
-    scrollcontainer: {
+    scrollContainer: {
         padding: 32,
-        // paddingTop: 30,
         backgroundColor: '#ffffff',
         borderRadius: 30,
     },
@@ -89,11 +80,11 @@ const styles = StyleSheet.create({
         marginBottom: 24,
     },
     h1: {
-        fontSize: 24, 
+        fontSize: 24,
         lineHeight: 26,
         letterSpacing: 0.06,
-        marginBottom: 27, 
-        
+        marginBottom: 27,
+
     },
     team: {
         fontSize: 16,
@@ -112,6 +103,9 @@ const styles = StyleSheet.create({
         height: Dimensions.get('screen').height,
         width: Dimensions.get('screen').width,
     },
+    scrollMargin: {
+        marginBottom: 300,
+    }
 })
 
 export default AboutScreen
